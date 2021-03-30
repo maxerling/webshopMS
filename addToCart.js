3bcb6df8525cd4b7d99bc6881c5dd47389f932fb
maxerling <maximilianerling@gmail.com>
2021-03-30 16:54:38 +0200

    add product to cart if product doenst exist

--- addToCart.js
+++ addToCart.js
@@ -2,50 +2,27 @@ function addToCart(productId) {
   let inCartArray = JSON.parse(localStorage.getItem("cart"));
   let allProducts = JSON.parse(localStorage.getItem("allProducts"));
   console.log(inCartArray);
-  inCartArray.push(allProducts[0]);
   let item;
   let index;
   if ((item = allProducts.find((item) => item.id == productId))) {
     index = inCartArray.findIndex((e) => e.id == item.id);
-    inCartArray[index].quantity + 1;
-    console.log(inCartArray[index].quantity);
+    if (!(index == -1)) {
+      console.log(index);
+      inCartArray[index].quantity++;
+    }
   } else {
+    index = allProducts.findIndex((e) => e.id == item.id);
+    let selectedPrd = {
+      id: allProducts[index].id,
+      title: allProducts[index].title,
+      image: allProducts[index].image,
+      price: allProducts[index].price,
+      quantity: 1,
+      units: allProducts[index].units,
+      category: allProducts[index].category,
+    };
+    inCartArray.push(selectedPrd);
   }
 
-  //product.id == products.product.id
-  /*for (let i = 0; i < allProducts.length; i++) {
-    if (productId == allProducts[i].id && inCartArray[i].quantity > 0) {
-      inCartArray[i].quantity++;
-    } else if (productId == inCartArray[i].id) {
-      for (let j = 0; j < allProducts.length; j++) {
-        let selectedPrd = {
-          id: allProducts[i].id,
-          title: allProducts[i].title,
-          image: allProducts[i].image,
-          price: allProducts[i].price,
-          quantity: 1,
-          units: allProducts[i].units,
-          category: allProducts[i].category,
-        };
-        inCartArray.push(selectedPrd);
-      }
-    }
-
-    console.log(inCartArray);
-  }*/
-
-  //inCartArray.push(sel);
+  localStorage.setItem("cart", JSON.stringify(inCartArray));
 }
-
-/**
- let selectedPrd = {
-            id: allProducts[i].id,
-            title: allProducts[i].title,
-            image: allProducts[i].image,
-            price: allProducts[i].price,
-            quantity: 1,
-            units: allProducts[i].units,
-            category: allProducts[i].category,
-          };
-          inCartArray.push(selectedPrd);
- */
