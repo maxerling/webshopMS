6a5c78d0e4052268678c541b71fafd0793cfbf29
maxerling <maximilianerling@gmail.com>
2021-03-30 18:13:34 +0200

    fixed add product to cartArray, if product exists +1 quantity

--- addToCart.js
+++ addToCart.js
@@ -2,27 +2,31 @@ function addToCart(productId) {
   let inCartArray = JSON.parse(localStorage.getItem("cart"));
   let allProducts = JSON.parse(localStorage.getItem("allProducts"));
   console.log(inCartArray);
-  let item;
-  let index;
-  if ((item = allProducts.find((item) => item.id == productId))) {
-    index = inCartArray.findIndex((e) => e.id == item.id);
-    if (!(index == -1)) {
-      console.log(index);
-      inCartArray[index].quantity++;
-    }
+  console.log();
+  console.log();
+  let item = allProducts.find((item) => item.id == productId);
+  let cartIndex = inCartArray.findIndex((e) => e.id == productId);
+  console.log(item);
+  console.log(cartIndex);
+  if (item != undefined && cartIndex != -1) {
+    console.log("if");
+    console.log(cartIndex);
+    inCartArray[cartIndex].quantity++;
   } else {
-    index = allProducts.findIndex((e) => e.id == item.id);
+    console.log("else");
+    let itemIndex = allProducts.findIndex((e) => e.id == productId);
     let selectedPrd = {
-      id: allProducts[index].id,
-      title: allProducts[index].title,
-      image: allProducts[index].image,
-      price: allProducts[index].price,
+      id: allProducts[itemIndex].id,
+      title: allProducts[itemIndex].title,
+      image: allProducts[itemIndex].image,
+      price: allProducts[itemIndex].price,
       quantity: 1,
-      units: allProducts[index].units,
-      category: allProducts[index].category,
+      units: allProducts[itemIndex].units,
+      category: allProducts[itemIndex].category,
     };
     inCartArray.push(selectedPrd);
   }
 
   localStorage.setItem("cart", JSON.stringify(inCartArray));
+  console.log(inCartArray);
 }
