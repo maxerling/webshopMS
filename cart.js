d48ea8928f6d811f27573f800ce796e3f71a202b
Karl Danielsson <karl.danielsson@yh.nackademin.se>
2021-03-31 17:05:16 +0200

    Added functionality to calc total price. Added new Json-data formatter

--- cart.js
+++ cart.js
@@ -7,22 +7,21 @@ let cartItems = [];
 const productArray = [];
 
 $.getJSON("data/products.json", function (json) {
-  console.log(json);
+  // console.log(json);
   for (let i = 0; i < json.length; i++) {
-    productArray.push(json[i]);
+    // productArray.push(json[i]);
+    let temp = {
+      productid: json[i].id,
+      producttitle: json[i].title,
+      productimage: json[i].image,
+      productprice: json[i].price,
+      productquantity: 3,
+    };
+    productArray.push(temp);
+    // console.log(temp);
   }
   localStorage.setItem("prodarray", JSON.stringify(productArray));
-  // localStorage.setItem("prod2", JSON.stringify(json[1]));
-  // localStorage.setItem("prod3", JSON.stringify(json[2]));
-  // localStorage.setItem("prod4", JSON.stringify(json[3]));
-  // localStorage.setItem("prod5", JSON.stringify(json[4]));
-  // localStorage.setItem("prod6", JSON.stringify(json[5]));
-  // localStorage.setItem("prod7", JSON.stringify(json[6]));
 });
-
-console.log(localStorage);
-console.log(productArray);
-// productArray.forEach((product) => createElementsForProduct(product));
 
 /*********************************************** */
 function getDataFromLocalStorage() {
@@ -32,24 +31,45 @@ function getDataFromLocalStorage() {
   }
   for (let i = 0; i < cartItems.length; i++) {
     $(".cart-table").append(htmlGenerator(cartItems[i]));
-    // console.log(cartItems[i]);
-    // console.log(cartItems);
   }
 }
 
 function htmlGenerator(data) {
   return `
-  <tr>
-  <td><img src="${data.image}" alt="" style="width: 75px;"></td>
-  <td>${data.title}</td>
-  <td><input min="1" name="quantity" value="1" type="number" id="cart-quantity"></td>
-  <td>SEK ${data.price.toFixed(2)}</td>
-  <td><button class="cart-remove-product"><i class="far fa-trash-alt class="trash-bin-image"></i></button></td>
-  </tr>
+  <tr class="quantity-tr">
+              <td><img src="${
+                data.productimage
+              }" alt="" style="width: 60px;"></td>
+              <td class = "td-title">${data.producttitle}</td>
+                <td class="quantity-td">
+                  <div class = "row mx-0 px-0">
+                    <input class="col-12 col-sm-12 col-md-6" min="1" name="quantity" value="${
+                      data.productquantity
+                    }" type="text" readonly id="cart-quantity">
+                    <div class="md-remove-one-product col-3 d-none d-sm-block"><i class="fas fa-minus"></i></i></div>
+                    <div class="md-add-one-product col-3 d-none d-sm-block"><i class="fas fa-plus"></i></div>
+                  </div>
+                  <div class="row">
+                    <div class="sm-remove-one-product col-3 col-sm-3 col-md-3 d-block d-sm-none"><i class="fas fa-minus"></i></div>
+                    <div class="sm-add-one-product col-3 col-sm-3 col-md-3 d-block d-sm-none"><i class="fas fa-plus"></i></div>
+                  </div>
+                </td>
+              <td>${data.productprice.toFixed(2)}</td>
+              <td>
+                <button class="cart-remove-product"><i class="far fa-trash-alt" class="trash-bin-image"></i></button>
+              </td>
+            </tr>
   `;
 }
 
-// $(".fa-plus").click("input", function () {});
+function calcPrice() {
+  let sum = 0;
+  for (let i = 0; i < cartItems.length; i++) {
+    sum += cartItems[i].productprice * cartItems[i].productquantity;
+  }
+  $("#total-price").html(sum.toFixed(2));
+}
+
 $(document).on("click", ".fa-plus", function () {
   let q = Number($(this).closest(".quantity-td").find("input").attr("value"));
   q++;
@@ -68,3 +88,4 @@ $(document).on("click", ".cart-remove-product", function () {
 });
 
 getDataFromLocalStorage();
+calcPrice();
