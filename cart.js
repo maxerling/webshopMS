26576dedd125a6e4c8266d64ed18d81afd80a25c
Karl Danielsson <karl.danielsson@yh.nackademin.se>
2021-04-06 11:51:22 +0200

    Added checkInputField() function. Also put everything inside document ready

--- cart.js
+++ cart.js
@@ -1,45 +1,50 @@
 "use strict";
 
-let cartItems = [];
+$(document).ready(function () {
+  let cartItems = [];
 
-/**
- *  Fetches data as an array with JSON object from local storage
- *  Appends html-elements using function htmlGenerator that creates html.
- */
-function getDataFromLocalStorage() {
-  let data = localStorage.getItem("cart");
-  if (cartItems != null) {
-    cartItems = JSON.parse(data);
-  }
-  for (let i = 0; i < cartItems.length; i++) {
-    $(".cart-table").append(htmlGenerator(cartItems[i]));
-  }
   /**
-   * Listener for input fields in cart that checks if value is positive.
-   * If not it resets the old value.
+   *  Fetches data as an array with JSON object from local storage
+   *  Appends html-elements using function htmlGenerator that creates html.
    */
-  $(".amount-changed").change(function () {
-    let newValue = Number(this.value.match(/^\d+$/));
-    let id = Number($(this).closest(".quantity-tr").attr("id"));
-    let tempProd = cartItems.filter((item) => {
-      return item.id == id;
-    })[0];
-    if (newValue && Number(newValue) > 0) {
-      setNewQuantity(id, newValue);
-      $(this).closest(".quantity-td").find("input").attr("value", newValue);
-    } else {
-      $(this).val(tempProd.quantity);
+  function getDataFromLocalStorage() {
+    let data = localStorage.getItem("cart");
+    if (cartItems != null) {
+      cartItems = JSON.parse(data);
     }
-  });
-}
+    for (let i = 0; i < cartItems.length; i++) {
+      $(".cart-table").append(htmlGenerator(cartItems[i]));
+    }
+    checkInputField();
+  }
 
-/**
- * Function that creates html table.
- * @param {object} data recieved from function getDataFromLocalStorage()
- * @returns a string
- */
-function htmlGenerator(data) {
-  return `
+  // /**
+  //  * Listener for input fields in cart that checks if value is positive.
+  //  * If not it resets the old value.
+  //  */
+  function checkInputField() {
+    $(".amount-changed").change(function () {
+      let newValue = Number(this.value.match(/^\d+$/));
+      let id = Number($(this).closest(".quantity-tr").attr("id"));
+      let tempProd = cartItems.filter((item) => {
+        return item.id == id;
+      })[0];
+      if (newValue && Number(newValue) > 0) {
+        setNewQuantity(id, newValue);
+        $(this).closest(".quantity-td").find("input").val(newValue);
+      } else {
+        $(this).val(tempProd.quantity);
+      }
+    });
+  }
+
+  /**
+   * Function that creates html table.
+   * @param {object} data recieved from function getDataFromLocalStorage()
+   * @returns a string
+   */
+  function htmlGenerator(data) {
+    return `
   <tr class="quantity-tr" id="${data.id}">
               <td><img src="${data.image}" alt="" style="width: 60px;"></td>
               <td class = "td-title">${data.title}</td>
@@ -62,116 +67,105 @@ function htmlGenerator(data) {
               </td>
             </tr>
   `;
-}
-
-/**
- * Function that calculates total price of current cart items.
- * Gets price and quantity from each object in cart.
- * Sets total-price html element to sum calculated.
- */
-function calcPrice() {
-  let sum = 0;
-  for (let i = 0; i < cartItems.length; i++) {
-    sum += cartItems[i].price * cartItems[i].quantity;
   }
-  $("#total-price").html(sum.toFixed(2));
-}
 
-/**
- * Function that takes an id and new quantity and sets that element to the new quantity.
- * @param {number} id id of product element of button clicked.
- * @param {number} qty new quantity after button clicked on -/+.
- */
-function setNewQuantity(id, qty) {
-  for (let i = 0; i < cartItems.length; i++) {
-    if (Number(cartItems[i].id) === id) {
-      cartItems[i].quantity = qty;
-      localStorage.setItem("cart", JSON.stringify(cartItems));
-      break;
+  /**
+   * Function that calculates total price of current cart items.
+   * Gets price and quantity from each object in cart.
+   * Sets total-price html element to sum calculated.
+   */
+  function calcPrice() {
+    let sum = 0;
+    for (let i = 0; i < cartItems.length; i++) {
+      sum += cartItems[i].price * cartItems[i].quantity;
     }
+    $("#total-price").html(sum.toFixed(2));
   }
-  calcPrice();
-}
 
-/**
- * Function that removes an object from cartItems array and
- * the corresponding html element.
- * Also updates local storage "cart" array accordingly.
- * Calls function calcPrice() to recalculate total price.
- * @param {number} id of product element of button clicked.
- */
-function removeFromList(id) {
-  for (let i = 0; i < cartItems.length; i++) {
-    if (Number(cartItems[i].id === id)) {
-      cartItems.splice(i, 1);
-      localStorage.setItem("cart", JSON.stringify(cartItems));
-      break;
+  /**
+   * Function that takes an id and new quantity and sets that element to the new quantity.
+   * @param {number} id id of product element of button clicked.
+   * @param {number} qty new quantity after button clicked on -/+.
+   */
+  function setNewQuantity(id, qty) {
+    for (let i = 0; i < cartItems.length; i++) {
+      if (Number(cartItems[i].id) === id) {
+        cartItems[i].quantity = qty;
+        $(`#${id}`)
+          .closest(".quantity-tr")
+          .find("input")
+          .attr("value", cartItems[i].quantity);
+        localStorage.setItem("cart", JSON.stringify(cartItems));
+        break;
+      }
     }
+    calcPrice();
   }
+
+  /**
+   * Function that removes an object from cartItems array and
+   * the corresponding html element.
+   * Also updates local storage "cart" array accordingly.
+   * Calls function calcPrice() to recalculate total price.
+   * @param {number} id of product element of button clicked.
+   */
+  function removeFromList(id) {
+    for (let i = 0; i < cartItems.length; i++) {
+      if (Number(cartItems[i].id === id)) {
+        cartItems.splice(i, 1);
+        localStorage.setItem("cart", JSON.stringify(cartItems));
+        break;
+      }
+    }
+    calcPrice();
+  }
+
+  /**
+   * Listener for plus sign button on each item in the cart.
+   * Finds id for product and updates elements quantity.
+   * Calls function setNewQuantity().
+   */
+  $(document).on("click", ".fa-plus", function () {
+    let q = Number($(this).closest(".quantity-td").find("input").attr("value"));
+    let id = Number($(this).closest(".quantity-tr").attr("id"));
+    q++;
+    console.log(q);
+    setNewQuantity(id, q);
+    $(this).closest(".quantity-td").find("input").val(q);
+  });
+
+  /**
+   * Listener for minus sign button on each item in the cart.
+   * Finds id for product and updates elements quantity.
+   * If quantity equals 1 trigger button with cart-remove-product class,
+   * else calls function setNewQuantity().
+   */
+  $(document).on("click", ".fa-minus", function () {
+    let q = Number($(this).closest(".quantity-td").find("input").attr("value"));
+    let id = Number($(this).closest(".quantity-tr").attr("id"));
+    if (q === 1) {
+      $(this)
+        .closest(".quantity-tr")
+        .find(".cart-remove-product")
+        .trigger("click");
+      return;
+    }
+    q--;
+    setNewQuantity(id, q);
+    $(this).closest(".quantity-td").find("input").val(q);
+  });
+
+  /**
+   * Listener for trash bin sign button for each item in the cart.
+   * Removes items from cart page using function removeFromList().
+   */
+  $(document).on("click", ".cart-remove-product", function () {
+    let id = Number($(this).closest(".quantity-tr").attr("id"));
+    $(this).closest(".quantity-tr").remove();
+    removeFromList(id);
+  });
+
+  // Runs on page load.
+  getDataFromLocalStorage();
   calcPrice();
-}
-
-/**
- * Listener for plus sign button on each item in the cart.
- * Finds id for product and updates elements quantity.
- * Calls function setNewQuantity().
- */
-$(document).on("click", ".fa-plus", function () {
-  let q = Number($(this).closest(".quantity-td").find("input").attr("value"));
-  let id = Number($(this).closest(".quantity-tr").attr("id"));
-  console.log(id);
-  q++;
-  setNewQuantity(id, q);
-  $(this).closest(".quantity-td").find("input").attr("value", q);
 });
-
-/**
- * Listener for minus sign button on each item in the cart.
- * Finds id for product and updates elements quantity.
- * If quantity equals 1 trigger button with cart-remove-product class,
- * else calls function setNewQuantity().
- */
-$(document).on("click", ".fa-minus", function () {
-  let q = Number($(this).closest(".quantity-td").find("input").attr("value"));
-  let id = Number($(this).closest(".quantity-tr").attr("id"));
-  if (q === 1) {
-    $(this)
-      .closest(".quantity-tr")
-      .find(".cart-remove-product")
-      .trigger("click");
-    return;
-  }
-  q--;
-  setNewQuantity(id, q);
-  $(this).closest(".quantity-td").find("input").attr("value", q);
-});
-
-/**
- * Listener for trash bin sign button for each item in the cart.
- * Removes items from cart page using function removeFromList().
- */
-$(document).on("click", ".cart-remove-product", function () {
-  let id = Number($(this).closest(".quantity-tr").attr("id"));
-  $(this).closest(".quantity-tr").remove();
-  removeFromList(id);
-});
-
-// $(".amount-changed").on("change", function () {
-//   console.log("hej");
-//   let test = $(this).val();
-//   console.log(test);
-//   // let q = Number($(this).closest(".quantity-td").find("input").attr("value"));
-//   // let id = Number($(this).closest(".quantity-tr").attr("id"));
-//   // console.log(id);
-//   // q++;
-//   // setNewQuantity(id, q);
-//   // $(this).closest(".quantity-td").find("input").attr("value", q);
-// });
-
-// $("input").live("change", function (e) {
-//   alert("Hello");
-// });
-
-// Runs on page load.
-getDataFromLocalStorage();
-calcPrice();
