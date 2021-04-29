4353435afe34ec8fc7a00a1793bf53ed544e73ee
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-04-29 09:15:01 +0200

    Add product to DB

--- admin-panel/js/add&edit-product.js
+++ admin-panel/js/add&edit-product.js
@@ -2,20 +2,21 @@
     SHOW UPLOADED IMAGE
 * ========================================== */
 /**
- * 
- * @param {*} input 
+ *
+ *
+ * @param {*} input
  */
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+// function readURL(input) {
+//     if (input.files && input.files[0]) {
+//         var reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('#imageResult')
-                .attr('src', e.target.result);
-        };
-        reader.readAsDataURL(input.files[0]);
-    }
-}
+//         reader.onload = function (e) {
+//             $('#imageResult')
+//                 .attr('src', e.target.result);
+//         };
+//         reader.readAsDataURL(input.files[0]);
+//     }
+// }
 
 // $(function () {
 //     $('#upload').on('change', function () {
@@ -26,8 +27,8 @@ function readURL(input) {
 /*  ==========================================
     SHOW UPLOADED IMAGE NAME
 * ========================================== */
-var input = document.getElementById( 'upload' );
-var infoArea = document.getElementById( 'upload-label' );
+// var input = document.getElementById( 'upload' );
+// var infoArea = document.getElementById( 'upload-label' );
 
 // input.addEventListener( 'change', showFileName );
 // function showFileName( event ) {
@@ -36,8 +37,105 @@ var infoArea = document.getElementById( 'upload-label' );
 //   infoArea.textContent = 'File name: ' + fileName;
 // }
 
-function renderImage(){
-    var input1 =  $('#upload').val();
-    console.log(input1);
-    $('#imageResult').attr('src', input1);
-}
+$("#upload-btn").click(function (e) {
+  e.preventDefault();
+  var input1 = $("#upload").val();
+  console.log(input1);
+  $("#imageResult").attr("src", input1);
+});
+
+///category
+$(document).ready(function () {
+
+  fetch("https://hakims-webshop.herokuapp.com/category/get")
+    .then((resp) => resp.json())
+    .then((data) =>
+      data.map((item) =>
+        $(".category").append(
+          `<option value="${item.id}">${item.name}</option>`
+        )
+      )
+    )
+    .catch((err) => console.log(err));
+
+    $('#input8').change(function () {
+        if($('#input8').val() != ""){
+            $('#input9').attr("disabled",false);
+        }else{
+            $('#input9').attr("disabled",true);
+        }
+    })
+   
+
+
+    $("form").submit(function (e) {
+        e.preventDefault();
+      console.log(parseInt($("input[name=featured]:checked").val()));
+        let product = {
+          title: $("#input1").val(),
+          brand: $("#input2").val(),
+          description: $("#input3").val(),
+          image: $("#upload").val(),
+          price: $("#input4").val(),
+          quantity: $("#input5").val(),
+          unit: $("#input6").val(),
+          featured: parseInt($("input[name=featured]:checked").val())
+        };
+      
+        if ($("#input8").val() != "") {
+          if ($("#input9").val() != "") {
+            product.category = [
+              {
+                id: $("#input7").val(),
+              },
+              {
+                id: $("#input8").val(),
+              },
+              {
+                id: $("#input9").val(),
+              },
+            ];
+          } else {
+            product.category = [
+              {
+                id: $("#input7").val(),
+              },
+              {
+                id: $("#input8").val(),
+              },
+            ];
+          }
+        } else {
+          product.category = [
+            {
+              id: $("#input7").val(),
+            },
+          ];
+        }
+      console.log(product.category);
+        fetch("https://hakims-webshop.herokuapp.com/product/add", {
+          method: "POST",
+          body: JSON.stringify(product),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then(function (res) {
+            console.log(res);
+            if (res.status == 200) {
+              return res.json();
+            } else {
+              alert("fel");
+            }
+          })
+          .then(function (product) {
+            console.log(product);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      });
+});
+
+
+
