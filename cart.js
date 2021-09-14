72c6473cb668ab17e6801cf7f7162fee697f516c
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-09-14 14:16:29 +0200

    bugg fix

--- cart.js
+++ cart.js
@@ -300,7 +300,7 @@ function createOrderRow(orderId) {
     .then((data) => {
       if (data == "Lagersaldona var mindre i lager än i beställningen") {
         alert(data);
-      } else if ((data == data) == "Produkt data är korrumperad") {
+      } else if (data == "Produkt data är korrumperad") {
         localStorage.removeItem("cart");
         localStorage.removeItem("totalPrice");
         alert(data);
